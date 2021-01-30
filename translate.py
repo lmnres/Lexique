a8561b60699487b57009915f43ade7cfa9696c0a
lmnres <luc.mouneyres@gmail.com>
2021-01-30 17:52:01 -0600

    premiere version

--- translate.py
+++ translate.py
@@ -7,41 +7,41 @@
 import os
 import os.path as op
 import pandas as pd
+import csv
 
-text_input = " Les coiffeurs ont coupé les cheveux des clients"
-text_output = ""
+text_input = "Les coiffeurs ont coupé les cheveux des clients"
+text_output = "Les coiffeurs et coiffeuses ont coupé les cheveux des client-es "
 
-# location of Lexique383.tsv file (adapt to your case!)
 LEXIQUE = op.join(os.getenv('HOME'), "/Users/mouneyres/Desktop/Lexique/Lexique383/Lexique383.tsv")
-
 lex = pd.read_csv(LEXIQUE, sep="\t")
 
-lex.head()
+#print(lex.loc[(lex.ortho == "coiffeurs")])
 
-# restreint la recherche à des mots de longueur comprises entre 5 et 8 lettres
-#subset = lex.loc[(lex.nblettres >= 5) & (lex.nblettres <= 8)]
-
-# separe les noms et les verbes dans deux dataframes:
-#noms = subset.loc[subset.cgram == 'NOM']
-#verbs = subset.loc[subset.cgram == 'VER']
-
-# sectionne sur la bases de la fréquence lexicale
-#noms_hi = noms.loc[noms.freqlivres > 50.0]
-#noms_low = noms.loc[(noms.freqlivres < 10.0) & (noms.freqlivres > 1.0)]
-
-#verbs_hi = verbs.loc[verbs.freqlivres > 50.0]
-#verbs_low = verbs.loc[(verbs.freqlivres < 10.0) & (verbs.freqlivres > 1.0)]
+# for word in text_input.split():
+#     if lex['ortho'].str.contains(word).any():
+#         ligne = lex.index[(lex.ortho == word)].tolist()
+#         data_row = lex.loc[(lex.ortho == word)]
+#
+#         print(ligne)
 
 for word in text_input.split():
-    #subset_word = lex.loc[(lex.ortho == word)]
-    #subset_word.lemme.to_csv('text_output.txt', index=False)
-    subset_word = (lex.loc[(lex.ortho == word)]).lemme
-    print(subset_word)
+    if lex['ortho'].str.contains(word).any():
+        data_row = lex.loc[(lex.ortho == word)]
+        if (data_row.iloc[0]["genre"] == "m") and (data_row.iloc[0]["nombre"] == "p"):
+            lemme = data_row.iloc[0]["lemme"]
+            print(word)
+            complementaire = lex.loc[(lex.lemme == lemme) & (lex.genre == "f") & (lex.nombre == "p")]
+            try :
+                print(complementaire.iloc[0]["ortho"])
+            except:
+                pass
+
+        elif (data_row.iloc[0]["genre"] == "f") and (data_row.iloc[0]["nombre"] == "p"):
+            print(word)
+        else:
+            print(word)
+    else:
+        print(word)
 
 
 
-    #lemme = lex.loc[lex.lemme == str(subset_word.lemme) ]
-    #print(lemme)
-    #if subset_word :
-        #if subset_word.genre == "m" & subset_word.nombre == "p" :
-            #supplement = lex.loc(lex.ortho == word)
