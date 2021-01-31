af3553944fdd08eb1263e3d76f671baac84ce840
lmnres <luc.mouneyres@gmail.com>
2021-01-31 01:06:42 -0600

    Troisième Version

--- translate.py
+++ translate.py
@@ -3,12 +3,18 @@ import os
 import os.path as op
 import pandas as pd
 
-text_input = "le cinéma a changé depuis que les acteurs ont montré leurs convictions sur les droits de la femme et l'égalité homme femme"
+text_input = "le cinéma a changé depuis que les acteurs ont montré leurs convictions sur les droits de la femme et l'égalité homme femme. ceux que nous connaissons sont aussi pour les droits de l'homme"
 text_output = []
 
 LEXIQUE = op.join(os.getenv('HOME'), "/Users/mouneyres/Desktop/Lexique/Lexique383/Lexique383.tsv")
 lex = pd.read_csv(LEXIQUE, sep="\t")
 
+# Remove dots , semi colon, etc
+text_input = text_input.replace('.','')
+text_input = text_input.replace(';','')
+text_input = text_input.replace(':','')
+
+# Translate Start
 for word in text_input.split():
     if lex['ortho'].str.contains(word).any():
         data_row = lex.loc[(lex.ortho == word)]
@@ -28,18 +34,28 @@ for word in text_input.split():
                 text_output.append("et " + complementaire.iloc[0]["ortho"])
             except:
                 pass
+        #elif (data_row.iloc[0]["cgram"] == "PRO:dem"):
+            #CEUX ET CELLES
         else:
             text_output.append(word)
     else:
         text_output.append(word)
-# Conditions homme femme replacé par femmme homme
+
+
+
+# Conditions Simples
     for i in range(len(text_output)-1):
         if text_output[i] == "homme" and text_output[i+1] == "femme":
             text_output[i]="femme"
             text_output[i+1]="homme"
         # remplace l'homme par l'humain
         if (text_output[i]== "l'homme" or text_output[i]== "l'Homme" or text_output[i]== "L'Homme" or text_output[i]== "L'homme"):
-            text_output[i]="l'humain"
+            text_output[i]="humain"
+        if text_output[i] == "maternelle":
+            text_output[i]="maternelle*"
+        if text_output[i] == "parternel":
+            text_output[i]="paternel*"
+
 
 print(text_output)
 
